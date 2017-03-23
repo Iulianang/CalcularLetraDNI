7230a6833495ac5649b85e92f8859c2f38038dcc
iulianang <iulian.anghel19@gmail.com>
2017-03-23 16:28:41 +0100

    v2

--- app/src/main/java/edu/calcularletradni/MainActivity.java
+++ app/src/main/java/edu/calcularletradni/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,12 +14,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void mostrarToas (String letra){
+        Toast toast = Toast.makeText(this, "Su letra es: "+letra,Toast.LENGTH_LONG);
+        toast.show();
+    }
+
     public void calcularDNI (View vista){
 
         EditText editText = (EditText)findViewById(R.id.numeros_dni);
         String dni = editText.getText().toString();
 
-        ObtenerLetraDNI obtenerLetraDNI = new ObtenerLetraDNI();
+        ObtenerLetraDNI obtenerLetraDNI = new ObtenerLetraDNI(this);
         obtenerLetraDNI.execute(dni); // execute es un metodo que llama a doInBackground
 
 
--- app/src/main/java/edu/calcularletradni/ObtenerLetraDNI.java
+++ app/src/main/java/edu/calcularletradni/ObtenerLetraDNI.java
@@ -1,26 +1,36 @@
 package edu.calcularletradni;
 
 import android.os.AsyncTask;
-import android.renderscript.ScriptGroup;
+
 import android.util.Log;
-import android.webkit.HttpAuthHandler;
+
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
+
 import java.net.URL;
 
 /**
  * Created by Administrador on 22/03/2017.
  */
 
- 
 
-public class ObtenerLetraDNI extends AsyncTask<String,Void,String >{
 
+public class ObtenerLetraDNI extends AsyncTask<String,Void,String > {
+
+    private MainActivity mainActivity;
     private static final String SERVLET_LETRA_DNI = "http://femxa-ebtm.rhcloud.com/CalcularLetraDni?dni=";
+
+    public ObtenerLetraDNI (MainActivity mainActivity) {
+        this.mainActivity = mainActivity;
+
+    }
+
+
+
+
 
     @Override
     protected String doInBackground(String... params) {
@@ -63,5 +73,6 @@ public class ObtenerLetraDNI extends AsyncTask<String,Void,String >{
         super.onPostExecute(letra);
         Log.d("MENSAJE", "La invocacion al servlet acabo");
         Log.d("MENSAJE", "Letra recibida = "+ letra);
+        mainActivity.mostrarToas(letra);
     }
 }
